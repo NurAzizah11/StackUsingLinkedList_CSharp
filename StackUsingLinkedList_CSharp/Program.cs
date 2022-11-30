10e34cca4234391412fe789bbf2073c48c45415c
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-11-30 16:04:55 +0700

    menambahkan Node tmp di public void display

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -47,6 +47,22 @@ namespace StackUsingLinkedList_CSharp
             top = top.next; //Make top point to the next node in sequence
         }
 
+        public void display()
+        {
+            Node tmp;
+            if (empty())    //If stack is empty
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                //Traverse the list from beggining till end
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+                Console.WriteLine();
+            }
+        }
+
     }
         }
 
